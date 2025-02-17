f92d3f26bfb85c91821c09ecbe1d8b54a48845e1
ejsusrojas <jesussantiago8596@gmail.com>
2025-02-16 23:09:43 -0400

    Add files via upload

---
+++ operaciones.js
@@ -0,0 +1,19 @@
+export function sumar(a, b) {
+    return a + b;
+}
+
+export function restar(a, b) {
+    return a - b;
+}
+
+export function multiplicar(a, b) {
+    return a * b;
+}
+
+export function dividir(a, b) {
+    if (b === 0) {
+        return 'error: división por cero';
+    } else {
+        return a / b;
+    }
+}
